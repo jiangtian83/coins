cb27e1284b1276c5afbe5d673e403afd182ee591
jiangtian <jiangtian@zpy360.com>
2020-06-06 14:32:09 +0800

    delete

--- git-webhook.php
+++ git-webhook.php
@@ -12,6 +12,8 @@ error_reporting(E_ALL);
 //git webhook 自动部署脚本
 $requestBody = file_get_contents("php://input"); //该方法可以接收post传过来的json字符串
 defined("LOG_DIR") or define("LOG_DIR", "./App/Runtime/Logs/Git/");
+defined("SRC_DIR") or define("SRC_DIR", "/tmp/coins");
+defined("DES_DIR") or define("DES_DIR", "/home/wwwroot/coins");
 if (empty($requestBody)) { //判断数据是不是空
 	die('send fail');
 }
@@ -23,15 +25,13 @@ $content = json_decode($content['payload'], true);
 if ($content['ref'] == 'refs/heads/master') {
 	file_put_contents(LOG_DIR . "git-webhook.log", "****写入日志****" . PHP_EOL, FILE_APPEND);
 	//PHP函数执行git命令
-	$bool = chdir('/home/wwwroot/tmp/coins');
+	$bool = chdir(SRC_DIR);
 	$re = shell_exec("ls -al");
 	$res = shell_exec('git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master');
 	file_put_contents(LOG_DIR . "git-content.log", ($bool ? 'T' : 'F') . PHP_EOL, FILE_APPEND);
 	file_put_contents(LOG_DIR . "git-content.log", $re . PHP_EOL, FILE_APPEND);
 	file_put_contents(LOG_DIR . "git-content.log", $res . PHP_EOL, FILE_APPEND);
-	$file = '/home/wwwroot/tmp/coins'; //旧目录
-	$newFile = '/home/wwwroot/coins'; //新目录
-	file_copy($file, $newFile);
+	file_copy(SRC_DIR, DES_DIR);
 	
 	$res_log = '-------------------------------------------' . PHP_EOL;
 	$res_log.= ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name']
