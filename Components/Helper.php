40a311a20008eccab8f535faf118dec3b5db911b
jiangtian <jiangtian@zpy360.com>
2020-06-06 23:15:39 +0800

    自定义打印内容函数

--- Components/Helper.php
+++ Components/Helper.php
@@ -15,7 +15,7 @@ if (!function_exists("put")) {
 		if (strpos($file, "../") === 0) $file = str_replace("../", dirname(getcwd()) . "/", $file);
 		elseif (strpos($file, "./") === 0) $file = str_replace("./", getcwd() . "/", $file);
 		elseif (strpos($file, "/") === 0) $file = str_replace("/", getcwd() . "/", $file);
-		if (strpos($file, "..") !== 0 || strpos($file, "./") !== 0 || strpos($file, "/") !== 0) $file = "/" . getcwd() . "/" . $file;
+		elseif (strpos($file, "..") !== 0 || strpos($file, "./") !== 0 || strpos($file, "/") !== 0) $file = "/" . getcwd() . "/" . $file;
 		echo $file;
 		if (!($dir = file_exists(dirname($file)))) mkdir($dir, 0777, true);
 		@file_put_contents($file, $data, $mod);
