020bf5c49ed5bc1bf5f828e4aad2d7fcb318c9e3
jiangtian <jiangtian@zpy360.com>
2020-06-06 14:44:15 +0800

    delete

--- git-webhook.php
+++ git-webhook.php
@@ -8,6 +8,7 @@
  * QQ: 284053253
  */
 
+ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 //git webhook 自动部署脚本
 $requestBody = file_get_contents("php://input"); //该方法可以接收post传过来的json字符串
@@ -26,6 +27,7 @@ if ($content['ref'] == 'refs/heads/master') {
 	file_put_contents(LOG_DIR . "git-webhook.log", "****写入日志****" . PHP_EOL, FILE_APPEND);
 	//PHP函数执行git命令
 	$bool = chdir(SRC_DIR);
+	
 	$re = shell_exec("ls -al");
 	$res = shell_exec('git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master');
 	file_put_contents(LOG_DIR . "git-content.log", ($bool ? 'T' : 'F') . PHP_EOL, FILE_APPEND);
