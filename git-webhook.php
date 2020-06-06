11647771866c84c5ed481b54976be7cb3d993ef9
jiangtian <jiangtian@zpy360.com>
2020-06-06 11:33:29 +0800

    git-webhook

--- git-webhook.php
+++ git-webhook.php
@@ -10,13 +10,14 @@
 
 //git webhook 自动部署脚本
 $requestBody = file_get_contents("php://input"); //该方法可以接收post传过来的json字符串
-file_put_contents("/home/wwwlogs/git-request-body.log", json_encode($requestBody, JSON_UNESCAPED_UNICODE) . PHP_EOL . PHP_EOL, FILE_APPEND);
+defined("LOG_DIR") or define("LOG_DIR", "./App/Runtime/Logs/Git/");
+file_put_contents(LOG_DIR . "git-request-body.log", json_encode($requestBody, JSON_UNESCAPED_UNICODE) . PHP_EOL . PHP_EOL, FILE_APPEND);
 if (empty($requestBody)) { //判断数据是不是空
 	die('send fail');
 }
 $content = json_decode($requestBody, true); //数据转换
 //若是主分支且提交数大于0
-file_put_contents("/home/wwwlogs/git-webhook.log", "****写入日志****" . PHP_EOL, FILE_APPEND);
+file_put_contents(LOG_DIR . "git-webhook.log", "****写入日志****" . PHP_EOL, FILE_APPEND);
 if ($content['ref'] == 'refs/heads/master') {
 	//PHP函数执行git命令
 	$res = shell_exec('cd /home/wwwroot/tmp/coins
@@ -31,7 +32,7 @@ if ($content['ref'] == 'refs/heads/master') {
 	$res_log.= ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name']
 		. '项目的' . $content['ref'] . '分支push' . $res . PHP_EOL;
 	//将每次拉取信息追加写入到日志里
-	file_put_contents("/home/wwwlogs/git-webhook.log", $res_log, FILE_APPEND);
+	file_put_contents(LOG_DIR . "git-webhook.log", $res_log, FILE_APPEND);
 }
 
 function file_copy($src, $dst) {
