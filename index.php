ad143ed292043f829a9aa2c6d36de974d2c81a7b
jiangtian <jiangtian@zpy360.com>
2020-06-06 12:45:47 +0800

    delete

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ define('APP_DEBUG',true);
 // define ( 'BIND_MODULE', 'Home' ); // 绑定Home模块到当前入口文件
 // 定义应用目录
 define('APP_PATH','./App/');
+
 echo 'git webhook!';
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
