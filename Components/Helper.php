05fe7ff012e7f6453fc4f9a7f278f43c6bcd6f75
jiangtian <jiangtian@zpy360.com>
2020-06-06 22:45:27 +0800

    自定义打印内容函数

---
+++ Components/Helper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2020/6/6
+ * Time: 下午10:37
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+
+if (function_exists("put")) {
+	function put ($file, $data, $mod)
+	{
+		if (is_array($data) || is_object($data)) $data = json_encode($data, JSON_UNESCAPED_UNICODE);
+		if (!($dir = file_exists(dirname($file)))) mkdir($dir, 0777, true);
+		@file_put_contents($file, $data, $mod);
+	}
+}
