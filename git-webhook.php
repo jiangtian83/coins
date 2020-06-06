4223dc75e95fd22eba3d63190c4a7dbaaf3f8de0
jiangtian <jiangtian@zpy360.com>
2020-06-06 13:57:38 +0800

    delete

--- git-webhook.php
+++ git-webhook.php
@@ -24,8 +24,8 @@ if ($content['ref'] == 'refs/heads/master') {
 	file_put_contents(LOG_DIR . "git-webhook.log", "****写入日志****" . PHP_EOL, FILE_APPEND);
 	//PHP函数执行git命令
 	chdir('/home/wwwroot/tmp/coins');
-	$re = shell_exec("ls -al");
-	$res = shell_exec('git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master');
+	$re = exec("ls -al");
+	$res = exec('git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master');
 	file_put_contents(LOG_DIR . "git-content.log", $res, FILE_APPEND);
 	$file = '/home/wwwroot/tmp/coins'; //旧目录
 	$newFile = '/home/wwwroot/coins'; //新目录
