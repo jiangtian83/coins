b26af38e0ab3ab8ff6255697798a5105512ae258
jiangtian <jiangtian@zpy360.com>
2020-06-06 22:35:06 +0800

    git

--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -37,12 +37,12 @@ const URL_COMPAT        =   3;  // 兼容模式
 // 类文件后缀
 const EXT               =   '.class.php'; 
 
-dump($_SERVER);
+var_dump($_SERVER);
 // 系统常量定义
 defined('THINK_PATH')   or define('THINK_PATH',     __DIR__.'/');
 defined('APP_PATH')     or define('APP_PATH',       dirname($_SERVER['SCRIPT_FILENAME']).'/');
 defined('APP_STATUS')   or define('APP_STATUS',     ''); // 应用状态 加载对应的配置文件
-defined('APP_DEBUG')    or define('APP_DEBUG',      false); // 是否调试模式
+defined('APP_DEBUG')    or define('APP_DEBUG',      true); // 是否调试模式
 
 if(function_exists('saeAutoLoader')){// 自动识别SAE环境
     defined('APP_MODE')     or define('APP_MODE',      'sae');
