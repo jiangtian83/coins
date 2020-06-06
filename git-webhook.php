cb8337267e7972f451583dcb10ac6dc4cd277d1a
jiangtian <jiangtian@zpy360.com>
2020-06-06 13:33:28 +0800

    delete

--- git-webhook.php
+++ git-webhook.php
@@ -18,7 +18,8 @@ if (empty($requestBody)) { //判断数据是不是空
 }
 $content = json_decode($requestBody, true); //数据转换
 if (!$content) parse_str(urldecode($requestBody), $content);
-file_put_contents(LOG_DIR . "git-content.log", json_encode($content, JSON_UNESCAPED_UNICODE), FILE_APPEND);
+$content = $content['payload'];
+file_put_contents(LOG_DIR . "git-content.log", $content, FILE_APPEND);
 die;
 //若是主分支且提交数大于0
 if ($content['ref'] == 'refs/heads/master') {
