a69e411190aecf482c3e8069bfc826b86ff61bf2
jiangtian <jiangtian@zpy360.com>
2020-06-07 14:51:38 +0800

    测试错误

--- ThinkPHP/Library/Think/Think.class.php
+++ ThinkPHP/Library/Think/Think.class.php
@@ -262,7 +262,7 @@ class Think {
     
     // 致命错误捕获
     static public function fatalError() {
-        Log::save();
+        Log::save();die;
         if ($e = error_get_last()) {
             switch($e['type']){
               case E_ERROR:
