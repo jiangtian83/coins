ddf571e126e7fb032e37be2818ae8d70c0889d2c
jiangtian <jiangtian@zpy360.com>
2020-06-07 17:52:14 +0800

    delete

--- ThinkPHP/Library/Think/Think.class.php
+++ ThinkPHP/Library/Think/Think.class.php
@@ -50,6 +50,7 @@ class Think {
           foreach ($mode['core'] as $file){
               if(is_file($file)) {
                 include $file;
+                echo APP_DEBUG;
                 if(!APP_DEBUG) $content   .= compile($file);
               }
           }
