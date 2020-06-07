6f04385d22f926e23f04f4f1e2bd8e87e52cf5b4
jiangtian <jiangtian@zpy360.com>
2020-06-07 18:21:10 +0800

    delete

--- App/Common/Conf/db.php
+++ App/Common/Conf/db.php
@@ -1,12 +1,12 @@
 <?php return array (
   'DB_TYPE' => 'mysql',
 
-  'DB_HOST' => 'localhost',
-  'DB_NAME' => 'haoid',
-  'DB_USER' => 'haoid',
-  'DB_PWD' => 'haoid.cn',
+  'DB_HOST' => '127.0.0.1',
+  'DB_NAME' => 'block_coins',
+  'DB_USER' => 'root',
+  'DB_PWD' => 'zhipingyun123',
   'DB_PORT' => '3306',
-  'DB_PREFIX' => 'yang_',
+  'DB_PREFIX' => 'trades_',
   'DB_FIELDS_CACHE' => true,
   'DB_CHARSET' => 'utf8',
 );
